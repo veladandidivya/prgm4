66e43c0829b642f1d55639149116f6e4003ff313
Divya Veladandi <divya.veladandi@otsi.com>
2023-11-23 17:18:15 +0530

    inital

---
+++ src/User.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+ function User(p) {
+  return (
+    <div>
+      <h1>name:{p.data.name}</h1>
+      <h1>name:{p.data.age}</h1>
+    </div>
+  )
+}
+export default User
